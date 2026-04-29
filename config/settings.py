f1922d5c59e289686b339ae2f2c4b0ed38d41462
fassadiop <mohamadoudiop2@esp.sn>
2026-04-29 15:34:07 +0000

    fix cors + csrf for production

--- config/settings.py
+++ config/settings.py
@@ -48,12 +48,15 @@ DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
-CORS_ALLOWED_ORIGINS = [
-    "http://samabulletin.edu.sn",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://samabulletin.edu.sn",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://saas-gestion-notes-backend.onrender.com",
 ]
 
 # Application definition
